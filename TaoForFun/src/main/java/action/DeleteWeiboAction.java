605aceeac0112f4d01a1260222a1eb77ae58b86d
天才少女梅子美\asus <q6q9qqq@sjtu.edu.cn>
2017-07-06 01:03:55 +0800

    update WeiboDeleteAction

--- TaoForFun/src/main/java/action/DeleteWeiboAction.java
+++ TaoForFun/src/main/java/action/DeleteWeiboAction.java
@@ -1,5 +1,30 @@
 package action;
 
-public class DeleteWeiboAction {
+import model.Weibo;
+import service.WeiboService;
 
+public class DeleteWeiboAction extends BaseAction {
+	private static final long serialVersionUID = 1L;
+	
+	private int weiboid;
+	private WeiboService weiboService;
+	
+	public void setWeiboid(int weiboid){
+		this.weiboid = weiboid;
+	}
+
+	public int getWeiboid(){
+		return weriboid;
+	}
+	
+	public String checkDeleteWeibo(){
+		if(session.get("weibo",weiboid) != null){
+			Weibo weibo = Weibo(weiboid);
+			weiboService.delete(weibo);
+			return "success";
+		}
+		else
+			return "input";
+	}
 }
+
